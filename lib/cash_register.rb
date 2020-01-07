c2e40ac18375814f27808016ce8d65d7206724dc
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2020-01-07 07:30:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,10 +25,19 @@ class CashRegister
     end
   end
   
-  def self.items
-    @items
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    @last_transaction = price * quantity
+    if quantity > 1
+   counter = 0
+   while counter < quantity
+     @items << title
+     counter += 1
+   end
+   else
+     @items << title
+   end
   end
-  
   
   
   
